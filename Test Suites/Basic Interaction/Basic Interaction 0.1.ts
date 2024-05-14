14bcc46a79da73aba2361f8ab37b8cd63aa6a701
naufalhaekall <naufalhaekall@localhost>
2024-05-14 14:08:06 +0700

    My First Commit

---
+++ Test Suites/Basic Interaction/Basic Interaction 0.1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Basic Interaction 0.1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c7473b4d-e7e0-4507-b9f0-11b058f28229</testSuiteGuid>
+   <testCaseLink>
+      <guid>a149fbe1-7bb8-4fc4-9bbc-c240e0138cee</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Basic Interaction/User Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
